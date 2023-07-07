04f0dcf4ffb58e21dd9a2ff0d761eb7af0072ee5
Hifzul Rashid <hifzul123@gmail.com>
2023-07-07 15:45:19 +0100

    Adding main entry point

---
+++ main.py
@@ -0,0 +1,8 @@
+# THIS IS MY FIRST PYTYHON PROECT CONENXT 4
+import sys
+import numpy as np
+
+# I WILL TEST COMMITTING NOW!!!
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
