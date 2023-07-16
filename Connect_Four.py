277bfdc1b4e5982d2caa505d36b38f247f4d12ee
Heybezayb <ishmail.alrashid@gmail.com>
2023-07-16 12:56:08 +0100

    Basic skeleton with emtpy methods

---
+++ Connect_Four.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class ConnectFour:
+    def __init__(self):
+        self.board = np.zeros((6,7))
+        self.player = 1
+
+    def show_board(self):
+        pass
+
+    def make_move(self, col):
+        pass
+
+    def is_valid_move(self, col):
+        pass
+
+    def check_win(self, row, col):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# THIS IS MY FIRST PYTYHON PROECT CONENXT 4
-import sys
 import numpy as np
 
-# I WILL TEST COMMITTING NOW!!!
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+def main():
+    pass
